05078eb01e577ccfa2bae8f6cf5ba0c3c2de8110
ksquaredcoding <ksquaredcoding@gmail.com>
2022-07-05 15:11:24 -0600

    Erasing notes and fixing css emergency-contact

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 let contacts = []
 
-/**
- * Called when submitting the new Contact Form
- * This method will pull data from the form
- * use the provided function to give the data an id
- * then add that data to the contacts list.
- * Then reset the form
- * *** hints:
- * *** push: resources/push.jpg
- */
 function addContact(event) {
   event.preventDefault()
   let form = event.target
@@ -25,20 +16,11 @@ function addContact(event) {
   form.reset()
 }
 
-/**
- * Converts the contacts array to a JSON string then
- * Saves the string to localstorage at the key contacts 
- */
 function saveContacts() {
   window.localStorage.setItem("contacts", JSON.stringify(contacts))
   drawContacts()
 }
 
-/**
- * Attempts to retrieve the contacts string from localstorage
- * then parses the JSON string into an array. Finally sets
- * the contacts array to the retrieved array
- */
 function loadContacts() {
   // @ts-ignore
   let storedContacts = JSON.parse(window.localStorage.getItem("contacts"))
@@ -47,11 +29,6 @@ function loadContacts() {
   }
 }
 
-/**
- * This function targets the contacts-list on the 
- * DOM and adds a new div element for each of the
- * contacts in the contacts array
- */
 function drawContacts() {
   let contactListElem = document.getElementById("contact-list")
   let contactsTemplate = ""
@@ -69,15 +46,7 @@ function drawContacts() {
   contactListElem.innerHTML = contactsTemplate
 }
 
-/**
- * This function is called with a contact id
- * and will use the id to find and remove the 
- * contact by their id from the list of contacts
- * *** hints: 
- * *** findIndex: resources/findIndex.jpg
- * *** splice: resources/splice.jpg
- * @param {string} contactId 
- */
+
 // @ts-ignore
 function removeContact(contactId) {
   let index = contacts.findIndex(contact => contact.id == contactId)
@@ -88,19 +57,10 @@ function removeContact(contactId) {
   saveContacts()
 }
 
-/**
- * Toggles the visibility of the AddContact Form
- */
 function toggleAddContactForm() {
   document.getElementById("new-contact-form")?.classList.toggle("hidden")
 }
 
-
-/**
- * Used to generate a random string id for mocked
- * database generated Id
- * @returns {string}
- */
 function generateId() {
   return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
 }
